a881f8bb0bc6374b92b8881ce55186f242221401
vkumarbadole <v.kumar.badole@accenture.com>
2024-08-13 14:39:46 +0530

    Create index.js

---
+++ .github/workflows/index.js
@@ -0,0 +1,24 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  throw new Error("Some error message");
+  core.debug("Debug Message");
+  core.warning("Warning message");
+  core.error("Error message");
+
+  const name = core.getInput("who_to_greet");
+
+  console.log(`Hello ${name}`);
+
+  const time = new Date();
+  core.setOutput("time", time.toTimeString());
+
+  core.exportVariable("HELLO_TIME", time);
+
+  core.startGroup("Logging github context");
+  console.log(JSON.stringify(github.context, null, 2));
+  core.endGroup();
+} catch (error) {
+  core.setFailed(error.message);
+}
